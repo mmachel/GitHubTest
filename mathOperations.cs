32ae79526dbcdcb33c0c70a3134ea0d31f1a01a7
MMachel <63281902+MMachel@users.noreply.github.com>
2020-04-15 17:55:19 +0200

    update of operations

--- mathOperations.cs
+++ mathOperations.cs
@@ -7,23 +7,32 @@ namespace Operations
     public static void Main(string[] args)
     {
       Console.WriteLine("Welcome to calculus basic Operations");
-      printInfo(Addition(32.0,64.0));
+      printInfo(Mult(32.0,64.0));
+    }
+    public static double Addition(double x, double y)
+    {
+      return x + y;
+    }
+    public static double Mult(double x, double y)
+    {
+      return x * y;
+    }
+    public static double Division(double x, double y)
+    {
+      return x / y;
+    }
+    public static double Subtration(double x, double y)
+    {
+      return x - y;
+    }
+    public static double Modulo(double x, double y)
+    {
+      return x % y;
+    }
+    public static void printInfo(double result)
+    {
+      Console.WriteLine("The result of the operation is "+result);
     }
   }
-  public static double Addition(double x, double y)
-  {
-    return x + y;
-  }
-  public static double Mult(double x, double y)
-  {
-    return x * y;
-  }
-  public static double Division(double x, double y)
-  {
-    return x / y;
-  }
-  public static void printInfo(double result)
-  {
-    Console.WriteLine("The result of the operation is "+result);
-  }
+
 }
